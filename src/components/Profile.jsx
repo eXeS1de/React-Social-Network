af2b05f47430613625ce750ae3e91cd64a744b4e
Tony Miller <Miller@yandex.ru>
2020-11-28 22:17:15 +0300

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <img src='https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg' alt='bg' />
+            <div>
+                ava + description
+            </div>
+            <div>
+                My Posts
+        <div>
+                    New Post
+        </div>
+                <div>
+                    Posts
+          <div>
+                        Post 1
+          </div>
+                    <div>
+                        Post 2
+          </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
